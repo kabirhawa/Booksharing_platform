a46455f1d43f01eb04e3d7b378de17ecbb7f4924
Rajputazad <Rajputazad337@gmail.com>
2023-08-06 13:07:48 +0530

    image api is padding , the rest is done

--- backend/index.js
+++ backend/index.js
@@ -16,6 +16,7 @@ app.use(
     credentials: true,
   })
 );
+
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Methods", "GET, POST, HEAD, OPTIONS, PUT, PATCH, DELETE");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-access-token, x-refresh-token, _id,authorization");
