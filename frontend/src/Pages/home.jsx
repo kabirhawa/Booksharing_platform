d55193dcc155557713032d8333743f639d03a6cf
Kabirahmed Hawawala <64394588+kabirahmedhawa@users.noreply.github.com>
2023-08-06 17:42:10 +0530

    and changes in backend

--- frontend/src/Pages/home.jsx
+++ frontend/src/Pages/home.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import { Typography } from "@mui/material";
+import React from "react";
 
 const Home = () => {
   return (
     <div>
-      
+      <Typography variant="h3">Home</Typography>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
